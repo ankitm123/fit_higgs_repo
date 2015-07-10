dfe9bbfc6ed5819a920b46c99e7c81d1b29e7c4c
Ankit Mohapatra <amohapatra2012@my.fit.edu>
2015-07-10 10:31:08 -0400

    Updated files

--- condor_scripts/rmf.sh
+++ condor_scripts/rmf.sh
@@ -3,3 +3,6 @@ rm -f test_skimmer_condor_file*.py
 cp file test/
 rm -f file*
 cp test/file .
+cp condor_vhtree_local.C condor_makeclass_local.C test/
+rm condor_vhtree*.C condor_makeclass_*.C
+cp test/condor_vhtree_local.C test/condor_makeclass_local.C .
