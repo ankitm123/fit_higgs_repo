3b356ef907e62e7eccd1eee889837c7c1fbf23ee
Ankit Mohapatra <amohapatra2012@my.fit.edu>
2015-07-10 13:26:10 -0400

    Updated changes

--- condor_scripts/condor_vhtree_local.C
+++
@@ -1,6 +0,0 @@
-void condor_vhtree_local(){
-  gROOT->ProcessLine(".L vhtree_local.C");
-  gROOT->ProcessLine("vhtree_local gem");
-  gROOT->ProcessLine("gem.Loop()");
-  gROOT->ProcessLine(".q");
-}
