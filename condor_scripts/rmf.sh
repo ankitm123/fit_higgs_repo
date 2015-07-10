6cb9690c76c7f346c1ea033c328276f0309be64f
Ankit Mohapatra <amohapatra2012@my.fit.edu>
2015-07-10 10:36:11 -0400

    Updated files

--- condor_scripts/rmf.sh
+++ condor_scripts/rmf.sh
@@ -6,3 +6,4 @@ cp test/file .
 cp condor_vhtree_local.C condor_makeclass_local.C test/
 rm condor_vhtree*.C condor_makeclass_*.C
 cp test/condor_vhtree_local.C test/condor_makeclass_local.C .
+rm -f vhtree_file*
